aa6ac3389f5d11a721b09d3615bda4ecc1209ee4
Daniel Chmielewski <dkchmielewski@gmail.com>
2020-06-27 11:54:44 -0400

    Corrected correctAnswers method in MathQuiz class, Changed result to showResults

--- src/MathQuiz.java
+++ src/MathQuiz.java
@@ -32,7 +32,7 @@ public class MathQuiz {
         return i == 123;
     }
 
-    public void result(boolean a, boolean b, boolean c) {
+    public void showResults(boolean a, boolean b, boolean c) {
         System.out.println("Odpowiedz 1: " + a);
         System.out.println("Odpowiedz 2: " + b);
         System.out.println("Odpowiedz 3: " + c);
@@ -42,13 +42,8 @@ public class MathQuiz {
     public void correctAnswers() {
         if (correctAnswers == 0) {
             System.out.println("Quiz score: 0/0");
-        }
-        if (correctAnswers == 1) {
-            System.out.println("Quiz score: 1/3");
-        } else if (correctAnswers == 2) {
-            System.out.println("Quiz score: 2/3");
-        } else if (correctAnswers == 3) {
-            System.out.println("Quiz score: 3/3");
+        } else (correctAnswers == 1) {
+            System.out.println("Quiz score: " + correctAnswers + " /3");
         }
     }
 }
